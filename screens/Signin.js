08fb2933f42d5f913e13fed578306d7ff68a4ea8
Álvaro <alvarohdezex@gmail.com>
2021-07-15 19:59:16 +0200

    fixed some styles

--- screens/Signin.js
+++ screens/Signin.js
@@ -1,8 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useRef } from 'react';
-import LottieView from 'lottie-react-native';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler'
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useRef } from "react";
+import LottieView from "lottie-react-native";
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 export function Signin({ navigation }) {
   const animation = useRef();
@@ -14,33 +14,36 @@ export function Signin({ navigation }) {
   }, [animation.current]);
 
   function onNavigateSignup() {
-    navigation.navigate('Signup');
+    navigation.navigate("Signup");
   }
 
   return (
     <View style={styles.container}>
-        <Text style={styles.title}>Work shift</Text>
-        <LottieView
-            ref={animation}
-            style={{
-              width: 400,
-              height: 400,
-              backgroundColor: '#000000',
-            }}
-            source={require('../animations/login.json')}
-          />
-        <TextInput placeholder="E-mail" style={styles.input} />
-        <TextInput placeholder="Password" style={styles.input} secureTextEntry />
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Sign in</Text>
+      <Text style={styles.title}>Work shift</Text>
+      <LottieView
+        ref={animation}
+        style={{
+          width: 400,
+          height: 250,
+          backgroundColor: "#000000",
+          alignSelf: "center",
+        }}
+        source={require("../animations/login.json")}
+      />
+      <TextInput placeholder="E-mail" style={styles.input} />
+      <TextInput placeholder="Password" style={styles.input} secureTextEntry />
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Sign in</Text>
+      </TouchableOpacity>
+      <View
+        style={{ flexDirection: "row", marginTop: "auto", alignSelf: "center" }}
+      >
+        <Text style={styles.signupText}>Don't you have an account?</Text>
+        <TouchableOpacity onPress={onNavigateSignup}>
+          <Text style={styles.signup}>Sign up</Text>
         </TouchableOpacity>
-        <View style={{ flexDirection: 'row', marginTop: 'auto' }}>
-          <Text style={styles.signupText}>Don't you have an account?</Text>
-          <TouchableOpacity onPress={onNavigateSignup}>
-            <Text style={styles.signup}>Sign up</Text>
-          </TouchableOpacity>
-        </View>
-        <StatusBar style="auto" />
+      </View>
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -48,46 +51,46 @@ export function Signin({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000000',
-    alignItems: 'center',
+    backgroundColor: "#000000",
     padding: 14,
     paddingTop: 40,
   },
   title: {
     fontSize: 25,
-    textAlign: 'center',
+    textAlign: "center",
     margin: 10,
-    color: '#ffffff',
-    textTransform: 'uppercase',
+    color: "#ffffff",
+    textTransform: "uppercase",
   },
   input: {
-    width: '100%',
+    width: "100%",
     height: 44,
-    backgroundColor: '#ffffff',
+    backgroundColor: "#ffffff",
     borderRadius: 4,
     marginBottom: 10,
     padding: 10,
     paddingLeft: 24,
   },
   button: {
-    backgroundColor: '#CB4437',
-    width: '100%',
+    backgroundColor: "#CB4437",
+    width: "100%",
     height: 44,
     borderRadius: 4,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 32,
   },
   buttonText: {
-    color: '#ffffff',
-    fontSize: 16
+    color: "#ffffff",
+    fontSize: 16,
   },
   signupText: {
     fontSize: 15,
-    color: '#ffffff',
+    color: "#ffffff",
     marginRight: 4,
   },
   signup: {
-    color: '#CB4437',
-    fontSize: 15
+    color: "#CB4437",
+    fontSize: 15,
   },
 });
